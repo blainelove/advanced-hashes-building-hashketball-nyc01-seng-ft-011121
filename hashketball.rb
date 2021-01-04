0ee758e5992674ad51046978b6d594fb68038f8d
Blaine R Love <blainelove94@gmail.com>
2021-01-04 01:25:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,13 @@
 # Write your code here!
 def game_hash
-  {:home
-  :away
+  {:home => {
+    :team_name => ''
+    :colors =>
+    :players =>[:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks]
+  }
+  :away =>{
+    :team_name =>
+    :colors =>
+    :players =>[:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks]
+  }
 }
